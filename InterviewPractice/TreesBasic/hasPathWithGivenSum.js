c043ca3b4d2771bca13f56180c312dc772ed80b4
salesni <71354362+salesni@users.noreply.github.com>
2021-04-02 17:04:25 -0600

    Create hasPathWithGivenSum.js

---
+++ InterviewPractice/TreesBasic/hasPathWithGivenSum.js
@@ -0,0 +1,27 @@
+//
+// Binary trees are already defined with this interface:
+// function Tree(x) {
+//   this.value = x;
+//   this.left = null;
+//   this.right = null;
+// }
+
+let findSum=(root,sum,acum)=>{
+    if(root){
+        if(isLeaf(root)){
+            return sum==acum+root.value;
+        }
+        else{
+            return findSum(root.right,sum,acum+root.value) ||
+                    findSum(root.left,sum,acum+root.value) 
+        }
+    }
+    return false;
+}
+
+let isLeaf=(node)=> node.right==null && node.left==null;
+
+function hasPathWithGivenSum(t, s) {
+    return findSum(t,s,0);
+
+}
